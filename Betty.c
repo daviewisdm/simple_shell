b6e37797ee14168757288a91e2a2e48eddf13126
Obi Chukwu <kennetho.onu@gmail.com>
2023-12-07 08:02:39 +0800

    Betty will be proud

--- Betty.c
+++ Betty.c
@@ -1,25 +1,36 @@
-#include <stdio.h>
+#include "shell.h"
+
+/**
+ * main - program that performs arithmetic functions on two arguments
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
 
 int main(int argc, char *argv[])
-{	
+{
 	int num1;
-        int num2;
-        int sum;
-        int product;
-        int difference;
+	int num2;
+	int sum;
+	int product;
+	int difference;
 
 	(void) argc;
-        (void)argv;
-	
+	(void)argv;
+
 	printf("Enter the first number: ");
 	scanf("%d", &num1);
 	printf("Enter the second number: ");
 	scanf("%d", &num2);
+
 	sum = num1 + num2;
 	product = num1 * num2;
 	difference = num1 - num2;
+
 	printf("Sum: %d\n", sum);
 	printf("Product: %d\n", product);
 	printf("Difference: %d\n", difference);
-	return 0;
+
+	return (0);
 }
