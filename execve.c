5d99aa4ed3db1bdc5873cfb8433e79a7d698a5a0
daviewisdm <wisdomdavie@gmail.com>
2023-12-13 00:27:37 -0800

    push updates

---
+++ execve.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#define MAX_COMMAND_LENGTH 256
+
+int main(void)
+{
+  char command[MAX_COMMAND_LENGTH];
+  char *args[] = {NULL, NULL};
+
+  while (1)
+  {
+      printf("#cisfun$ ");
+      fgets(command, MAX_COMMAND_LENGTH, stdin);
+
+      /* Remove the newline character at the end of the command */
+      command[strcspn(command, "\n")] = 0;
+
+      args[0] = command;
+
+      /* Execute the command */
+      if (execve(command, args, NULL) == -1)
+      {
+          perror("./shell");
+      }
+  }
+
+  return 0;
+}
